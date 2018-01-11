67cc75aa888668ae2bd849b3840c613dcce1db75
Sri <sprasad1@ucvts.tec.nj.us>
2018-01-11 19:25:31 +0000

    second commit / added senerio 2

--- spgame.py
+++ spgame.py
@@ -15,15 +15,43 @@ print(directions)
 print("Begin your adventure")
 
 #scenario one
-A= "A) run into the bushes"
-B= "B) let him eat you"
-C= "C) jump to the ground and hope he can't see you"
-D= "D) climb a tree next to you#inputting their choice
+A= "A)run into the bushes"
+B= "B)let him eat you"
+C= "C)jump to the ground and hope he can't see you"
+D= "D)climb a tree next to you"
+#inputting their choice
 print("You are walking through the Amazon Rain Forest, all of a sudden Abominable Snowman jumps into your path, what do you do?")
-x = input("Do you...", A, B, C, D)
-if x = A:
+print(A, B, C, D)
+x = input("Do you... ")
+if x == "A":
     print("You manage to evade the monster by running into the bushes, but you don't know for how long.")
-if x = B:
-    print("You let him eat you, you die and lose the game")
+if x == "B":
+    print("You let him eat you and die")
+if x == "C":
+    print("He finds you and eats you, you die")
+if x == "D":
+    print("He knocks down the tree and eats you, you die")
+if x == "B" or x == "C" or x == "D":
+    print("you lose the game    !")
+
+#scenario 2
+A = "A)you try to eat the cobra"
+B = "B)you kick the cobra in the face, and run away"
+C = "C)you try to scare the cobra away"
+D = "D)you bellyflop onto the snake"
+#input their choice
+print("You get up from the ground, to see a king cobra in front of you, what should you?")
+print(A, B, C, D)
+x = input("Do you...")
+if x == "A":
+    print("The snake bites you mouth, and kills you")
+if x == "B":
+    print("You knock the snake out and run away")
+if x == "C":
+    print("He lunges at you, bites you, and kills you")
+if x == "D":
+    print("The cobra bites in mid air, you die")
+if x == "A" or "C" or "D":
+    print("you lose the game")
 
 
