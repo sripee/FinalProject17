3ca7f09c9f401e08e82ad3a603e974029c378bd1
Sri <sprasad1@ucvts.tec.nj.us>
2018-01-26 17:30:44 +0000

    Final

--- spgame.py
+++ spgame.py
@@ -36,17 +36,29 @@ D= "D)climb a tree next to you"
 print("You are walking through the Amazon Rain Forest, all of a sudden Abominable Snowman jumps into your path, what do you do?")
 print(A, B, C, D)
 x = input("Do you... ")
-if x == "A":
-    print("You manage to evade the monster by running into the bushes, but you don't know for how long.")
-if x == "B":
-    print("You let him eat you and die")
-if x == "C":
-    print("He finds you and eats you, you die")
-if x == "D":
-    print("He knocks down the tree and eats you, you die")
-if x == "B" or x == "C" or x == "D":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "A":
+        print("You manage to evade the monster by running into the bushes, but you don't know for how long.")
+        break
+    if x == "B":
+        print("You let him eat you and die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("He finds you and eats you, you die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("He knocks down the tree and eats you, you die")
+        print("you lose the game")
+        sys.exit()
+        break
+
+
+
 
 #scenario 2
 A = "A)you try to eat the cobra"
@@ -56,18 +68,28 @@ D = "D)you bellyflop onto the snake"
 #input their choice
 print("You get up from the ground, to see a king cobra in front of you, what should you?")
 print(A, B, C, D)
-x = input("Do you...")
-if x == "A":
-    print("The snake bites you mouth, and kills you")
-if x == "B":
-    print("You knock the snake out and run away")
-if x == "C":
-    print("He lunges at you, bites you, and kills you")
-if x == "D":
-    print("The cobra bites in mid air, you die")
-if x == "A" or x == "C" or x == "D":
-    print("you lose the game")
-    sys.exit()
+x = input("Do you... ")
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "A":
+        print("The snake bites you mouth, and kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "B":
+        print("You knock the snake out and run away")
+        break
+    if x == "C":
+        print("cobra kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("The cobra bites in mid air, you die")
+        print("you lose the game")
+        sys.exit()
+        break
+
 
 #scenario 3
 A = "A)go into the cave"
@@ -77,25 +99,35 @@ D = "D)curl up into a ball and cry"
 #input their choice
 print("You walk out of the forest, and see a snowy mountain,but there is also a cave under it, what do you do?")
 print(A, B, C, D)
-x = input("Do you...")
-if x == "A":
-    print("You walk into the cave, but inside the abominable snowman is waiting you, he rips you in half")
-if x == "B":
-    print("You walk into a bear trp, and die")
-if x == "C":
-    print("You climb the mountain, but see the Snowman behind you, though you are safe for now")
-if x == "D":
-    print("The snow man finds you, and eats you")
-if x == "A" or x == "B" or x == "D":
-    print("you lose the game")
-    sys.exit()
+x = input("Do you... ")
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "C":
+        print("You climb the mountain, but see the Snowman behind you, though you are safe for now")
+        break
+    if x == "B":
+        print("You walk into a bear trp, and die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "A":
+        print("You walk into the cave, but inside the abominable snowman is waiting you, he rips you in half")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("The snow man finds you, and eats you")
+        print("you lose the game")
+        sys.exit()
+        break
+
 
 #They get powerup for getting question right
 print("You come across the skip question powerup, you can only use it once")
 powerup.append("skip")
 print(powerup)
 
-#scenario 3
+#scenario 4
 A = "A)jump off the mountain"
 B = "B)climb back down the mounatin"
 C = "C)eat your arm"
@@ -104,17 +136,26 @@ D = "D)take the trail"
 print("There is a trail ontop of the mountain, and the Abmominable Snowman is catching up to you, what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("You fall to your death and die")
-if x == "B":
-    print("The Snowman catches you and kills you")
-if x == "C":
-    print("You are dumb and die")
-if x == "D":
-    print("You take the trail, and go to the other side of the mountain")
-if x == "A" or x == "B" or x == "C":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "D":
+        print("You take the trail, and go to the other side of the mountain")
+        break
+    if x == "B":
+        print("The Snowman catches you and kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("You are dumb and die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "A":
+        print("You fall to your death and die")
+        print("you lose the game")
+        sys.exit()
+        break
 
 #scenario 4
 A = "A)pick up an apple and eat it"
@@ -128,25 +169,33 @@ if "skip" in powerup:
     s = input("do you want to use your powerup? write yes or no")
     if s == "yes":
         x == "A"
+        print("put answer A")
         powerup.remove("skip")
 
     else:
         print(A, B, C, D)
         x = input("Do you...")
-else:
-    print(A, B, C, D)
-    x = input("Do you...")
+
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
     if x == "A":
         print("You eat the apple, and fill your stomach, you made the right choice")
+        break
     if x == "B":
         print("The Snowman jumps over the tree and eats you")
-    if x == "C":
-        print("The snowman knocks down the tree and eats you")
-    if x == "D":
-        print("You are stupid, you throw away food, and die of starvation")
-    if x == "D" or x == "B" or x == "C":
         print("you lose the game")
         sys.exit()
+        break
+    if x == "C":
+        print("The snowman knocks down the tree and eats you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("The snowman knocks down the tree and eats you")
+        print("you lose the game")
+        sys.exit()
+        break
 
 #scenario 5
 A = "A)eat the eggs in the birds nest"
@@ -157,17 +206,28 @@ D = "D)fart on the eggs"
 print("You are walking down the mountain when you see a bird's nest full of eggs, what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("The motherbird comes back, and claws your eyes out, and you die")
-if x == "B":
-    print("You calmly walk by the nest avoiding trouble")
-if x == "C":
-    print("The motherbird kills you for killing her children")
-if x == "D":
-    print("You are stupid, why would you fart on the eggs")
-if x == "A" or x == "D" or x == "C":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "B":
+        print("You calmly walk by the nest avoiding trouble")
+        break
+    if x == "A":
+        print("The motherbird comes back, and claws your eyes out, and you die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("The motherbird kills you for killing her children")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("You are stupid, why would you fart on the eggs")
+        print("you lose the game")
+        sys.exit()
+        break
+
+
 
 #scenario 6
 A = "A)run away from the tiger"
@@ -178,17 +238,27 @@ D = "D)jump up an down"
 print("You come into a clearing where you see a tiger, what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("The tiger eats you")
-if x == "B":
-    print("The tiger mauls you to death")
-if x == "C":
-    print("The tiger gets scared of you, and runs away")
-if x == "D":
-    print("You are stupid, why would you jump up and down")
-if x == "A" or x == "D" or x == "B":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "C":
+        print("The tiger gets scared of you, and runs away")
+        break
+    if x == "B":
+        print("The tiger mauls you to death")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "A":
+        print("The tiger eats you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("You are stupid, why would you jump up and down")
+        print("you lose the game")
+        sys.exit()
+        break
+
 
 #scenario 7
 A = "A)attack the lion"
@@ -199,17 +269,28 @@ D = "D)fart in the lions face"
 print("You escape the tiger, but you come across a lion what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("the lion kills you")
-if x == "B":
-    print("The lion eats you instead")
-if x == "C":
-    print("The lion bites off your hand and kills you")
-if x == "D":
-    print("The lion suffocates on your fart and passes out")
-if x == "A" or x == "B" or x == "C":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "D":
+        print("The lion suffocates on your fart and passes out")
+        break
+    if x == "B":
+        print("The lion eats you instead")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("The lion bites off your hand and kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "A":
+        print("the lion kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+
+
 
 #scenario 8
 A = "A)eat the berries"
@@ -220,17 +301,28 @@ D = "D)burn the berries"
 print("You get hungry and see a bush of berries, what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("You eat the berries, but they are poisonous you die")
-if x == "B":
-    print("You dodged a bullet, the berries were poisonous")
-if x == "C":
-    print("The berry juice is poisonous you die")
-if x == "D":
-    print("You burn the berries, but the smoke is poisoned you die")
-if x == "A" or x == "D" or x == "C":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "B":
+        print("You dodged a bullet, the berries were poisonous")
+        break
+    if x == "A":
+        print("You eat the berries, but they are poisonous you die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("The berry juice is poisonous you die")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("You burn the berries, but the smoke is poisoned you die")
+        print("you lose the game")
+        sys.exit()
+        break
+
+
 
 #scenario 9
 A = "A)kick it away"
@@ -241,19 +333,29 @@ D = "D)cut off your leg"
 print("You find a sword on the ground what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("You are stupid, why would you kick it away")
-if x == "B":
-    print("You are smart, you pick up the sword in case you run into danger")
-if x == "C":
-    print("You are stupid, why would you break apotential weapon")
-if x == "D":
-    print("You are stupid, why would you cut off your leg")
-if x == "A" or x == "D" or x == "C":
-    print("you lose the game")
-    sys.exit()
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "B":
+        print("You are smart, you pick up the sword in case you run into danger")
+        break
+    if x == "A":
+        print("You are stupid, why would you kick it away")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("You are stupid, why would you break apotential weapon")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("You are stupid, why would you cut off your leg")
+        print("you lose the game")
+        sys.exit()
+        break
 
-#scenario 7
+
+#scenario 10
 A = "A)fight the snowman with your sword"
 B = "B)run away from the snowman"
 C = "C)beg the snowman for mercy"
@@ -262,19 +364,25 @@ D = "D)kill yourself with the sword"
 print("You run into the clearing, and you see the Abominable Snowman on the other side, what do you do?")
 print(A, B, C, D)
 x = input("Do you...")
-if x == "A":
-    print("You fight the snowman with your sword and kill him, Congratulations, you have won the game!!")
-    sys.exit()
-if x == "B":
-    print("The snowman catches you and eats")
-if x == "C":
-    print("He does not give you mercy, and kills you")
-if x == "D":
-    print("You are stupid why would you kill yourself")
-if x == "D" or x == "B" or x == "C":
-    print("you lose the game")
-    sys.exit()
-
-
-
+while x != "A" or x != "B" or x != "C" or x != "D" is True:
+    x = input("put your final answer")
+    if x == "A":
+        print("You fight the snowman with your sword and kill him, Congratulations, you have won the game!!")
+        sys.exit()
+        break
+    if x == "B":
+        print("The snowman catches you and eats")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "C":
+        print("He does not give you mercy, and kills you")
+        print("you lose the game")
+        sys.exit()
+        break
+    if x == "D":
+        print("You are stupid why would you kill yourself")
+        print("you lose the game")
+        sys.exit()
+        break
 
